f3fcf377afd3c02ff48ee1d22b5ee3718f65ef6b
Noel Brizuela <nogutier@ucsd.edu>
2021-10-07 15:03:38 -0700

    added internal wave class

--- modview/phystools.py
+++ modview/phystools.py
@@ -1,4 +1,5 @@
 import numpy as np; import xarray as xr; import pandas as pd; 
+import sympy as sym;
 
 def lon_to_x(lon, lat):
     x = lon*110e3*np.cos(np.mean(lat)); x = x - np.mean(x);
@@ -8,10 +9,10 @@ def lat_to_y(lat):
     y = lat*110e3; y = y - np.mean(y);
     return y
     
-def hydrostatic_pressure(z,density,elevation=0):
-    # Integrate density to calculate hydrostatic pressure
-    
-
+def inertial_freq(lat):
+    f = 4*np.pi*np.sin(lat/180*np.pi)/24/3600;
+    return f
+   
 def geostrophy(lat, lon, depth, density, ssh='none'):
     # Calculate geostrophic shear/currents from density along a transect.
     # either lat or lon  need to be sequence with a single element
@@ -36,5 +37,62 @@ def geostrophy(lat, lon, depth, density, ssh='none'):
         drho = drho / np.gradient(y); 
     elif solve_for == 'v':
         drho = drho / np.gradient(x); 
+
+class internal_wave:
+    def __init__(self, lat, N2=1e-5 ):
+        self._fk = dict();  # vector [freq, kx, ky, kz].
+        self._Nsquared = N2;
+        self.lat = lat;
     
+    @property
+    def Nsquared(self):
+        print('getting N2');
+        return self._Nsquared
+    
+    @Nsquared.setter
+    def Nsquared(self, n2):
+        # Set value to argument n2
+        self._Nsquared = n2;
+    
+    @property
+    def fk(self):
+        print('getting wave frequency and wavenumbers')
+        return self._fk
+    
+    @fk.setter
+    def fk(self, wave_properties):
+        # Run dis_rel using fk_vec input. 
+        test_func = self.dispersion_relation()
+        
+        test_input = []; solve_for = []; var_solved = [];
+        for key, value in wave_properties.items():
+            if np.isnan(value):
+                    var_solved.append(key);
+                    solve_for.append(sym.symbols(key)); # values missing
+            else: 
+                test_input.append( (sym.symbols(key), value) ) # values known
+        try_func = test_func.subs(test_input);
+        solution = sym.solve(try_func, solve_for)
+        
+        if solution[0].is_real:
+            wave_properties[var_solved[0]] = solution[0];
+            self._fk = wave_properties; 
+        else:
+            print('Incompatible values: imaginary wavenumber');
+            pass
+
+    def dispersion_relation(self):
+        # Symbolic representation of dispersion relation
+        omega, kx, ky, kz = sym.symbols('omega kx ky kz')
+        norm = kx**2 + ky**2 + kz**2;
+        relation = - omega**2 + self.Nsquared*(kx**2 + ky**2)/norm \
+                    + inertial_freq(self.lat)**2 * kz**2 / norm; 
+        return relation
+    
+    def group_vel(self, direction='z'):   
+        pass
+        
+        
+        
+        
         
