ae64bc313e2a914500fde8be3a977ec72b21a4eb
Noel Brizuela <nogutier@ucsd.edu>
2021-11-25 12:09:31 -0800

    comentarios explicando codigo

--- modview/DataBase_functions.py
+++ modview/DataBase_functions.py
@@ -14,6 +14,10 @@ c = conn.cursor()
 
 
 def Prepare_id(**kwargs):
+    # This function runs through items in **kwargs and uses them to build an sql statement to execute.
+    # At the end, it returns all database rows whose column names and entries match with **kwargs 
+    # Example usage so far has only been **kwargs = {t0='2018 Sep 15'}. Namely, return all observational objects
+    # with the specific t0.
     Data = "SELECT * FROM OBJ_ID"
     i = 0
     for key, value in kwargs.items():
@@ -23,11 +27,12 @@ def Prepare_id(**kwargs):
             Data += " AND "
         Data += "{}='{}'".format(key, value)
         i += 1
-    Data += ";"
+    Data += ";" 
     return c.execute(Data)
 
 
 def format_change(DICT):
+    # This function turns 
     limits_keys = ['t0','t1','lon','lat']
     for n in range(len(DICT)):
         DICT[n]['limits'] = {key: DICT[n][key] for key in DICT[n] if key in limits_keys}
@@ -40,18 +45,23 @@ def id_maker(**kwargs):
     return A
 
 def Data_extraction(obj_id, n, **kwargs):
-    combiner_id = obj_id[n]['COMBINER_ID']
+    ''' obj_id is a list of dictionaries (returned by Prepare_id) each of which includes
+    all fields that identify observational objects matching some request. 
+    n is the index of the item of interest within obj_id. '''
+    
+    combiner_id = obj_id[n]['COMBINER_ID'] # use combiner table to pull datasources that make up obj_id[n]
     common_str = 'SELECT * FROM COMBINER AS c INNER JOIN DATASOURCES as d ON c.Data_obj = d.Data_obj WHERE c.COMBINER_ID = %d'
     i = 1
-    for key, value in kwargs.items():
-        common_str += " AND d."
+    for key, value in kwargs.items(): # examples so far have not used **kwargs at all. 
+        common_str += " AND d." # add new clauses to database statement to execute
         common_str += "{}='{}'".format(key, value)
         i += 1
     common_str += ";"
-    c.execute(common_str %combiner_id)
+    c.execute(common_str %combiner_id) # execute statement (returns all entries in datasources table that corresponds to obj_id[0]
     return [dict(row) for row in c.fetchall()] 
 
 def path_maker(DataSources, data_dir):
+    # Take output from data_extraction and return a dictionary that can be run through loader.assemble
     Dir = {}
     for kk in range(len(DataSources)):
         if DataSources[kk]['Source'] not in Dir:
